5d0d0574056f5cd03401f080942d5cf1942dadbb
Darren <kjchen@protonmail.ch>
2022-08-31 06:02:51 +0800

    INIT COMMIT

---
+++ JieFangXS.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+
+//                                           .~~:                                                                          
+//                                          !GBBBY.                                                                        
+//                                          Y#BBBB^..           ..........................................             ..  
+//                                          Y#BBBBJ??.     ^7Y5PPGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGPP5J!.      !??~ 
+//                                          Y#BBBBYJ?.   :YB#####BGGBBBB###BGGB#BB#BPPG#BBBBGGB###BBB#BGGB#####G?     !JJ! 
+//                                          Y#BBBBYJ?.  .P#BBBG?^: ?#BBBBY~:.:GBBBG^  .J#BB#7 .^?G#BBBG:.:!5BBBB#!    !JJ! 
+//                                          Y#BBBBYJ?.  :BBBBG^    Y#BBB7    ^BBB#J    :BBB#J    :PBBBB:    ?#BB#J    !JJ! 
+//                                          Y#BBBBYJ?.  :BBB#J     Y#BBB:    ^BBB#?    :BBB#J     ?#BBB^    :BBB#J    !JJ! 
+//                                          Y#BBBBYJ?.  .P#B#5     7#BBB^    .P#B#Y    ^BB##7     Y#B#P.    ~BB##!    !JJ! 
+//                                          Y#BBBBYJ?.   :5B##J.    7PB#P^    :YGBB~   Y#BP!    .JBBGY:   .!G#BP!     !JJ! 
+//                                          Y#BBBBYJ?.     :!YGG5?77!7YG##PJ777!?5BBYJP#GY7!77?5B#BP?!77?YGB5?^       !JJ! 
+//                                          Y#BBBBYJ?.    :75GGJ~^:::?PB#5!^::.!YGBG?!YBBPJ^..:^7G#G5!...:~YBG5?:     !JJ! 
+//                                          Y#BBBBYJ?.   !B##B~    .5###J     7B##G^   7#B#G^    :P###J     ?###G^    !JJ! 
+//                                          Y#BBBBYJ?.  .G#B#Y     ?#BBB^    .GBB#Y    .GBB#Y     7#BBB^    .GBB#Y    !JJ! 
+//                                          Y#BBBBYJ?.  .GBB#Y     J#BBB:    :BBB#J    .PBB#5     !#BBB~    .GBB#Y    !JJ! 
+//                                          Y#BBBBYJ?.  .GBB#Y     J#BBB:    :BBB#J    .P#B#5     !#BB#~    .PBB#5    !JJ! 
+//                                          Y#BBBBYJ?.  .GBB#Y     ?#BBB:    :BBB#J     P#B#5     !#BB#~    .PBB#5    !JJ! 
+//                                          Y#BBBBYJ?.  .GBB#Y     ?#BBB^    .GBB#J     P#B#5     !#BB#~    .P#B#5    !JJ! 
+//                                          Y#BBBBYJ?.  .GBB#Y     ?#BBB^    .GBB#Y     P#B#5     !#BB#~    .P#B#5    !JJ! 
+//                                          Y#BBBBYJ?.  .G#B#5     ?#BBB^    .G#B#Y     P#B#P     !#BB#!    .P#B#5    !JJ! 
+//                                          Y#BBBBYJ?.  .PBGBY     7BGGG^    .PBGBY     5BGB5     ~BGGB~    .PBGBY    !JJ! 
+//                                          Y#BBBBYJ?.   .....      ....      .....     .....      ....      .....    !JJ! 
+//                                          Y#BBBBYJ?.~!!!!!!.   !7^     ~!!!!!~:      ~!!!!!~.     ^7!.     :~!!!!~. !JJ! 
+//                                          Y#BBBBYJ?.5B~~~~^. :5Y7G?   .GG!!!!5P.     5B^^^^7G7   7P!YP^  .YP!^::^?G!!JJ! 
+//                                          Y#BBBBYJJ.5B!!!!: !G5~~?BP^ .GP~~~~JG~ ..  5G.   :G5 :5G7~~5B7 :B5.    ^BJ!JJ! 
+//                                          Y#BBBB!^^ JY     !5^:::::?5^ YY7777?J: 7Y. ?5????J7.:Y?:::::~57 :?J?7??J! :~^. 
+//                                          Y#BBBB:                                                                        
+//                                          Y#BBBB:                                                                        
+//                                          Y#BBBB:                                                                        
+//                      .:~!7?JJJJ?7!~^.    Y#BBBB:                                      .^~!7?JJJJ?7!~:.                  
+//                  ^75G&@@@@@@@@@@@@@@&B5?^YBBBBB:                                 .~?5B&@@@@@@@@@@@@@@&G57^              
+//              .!5#@@@&BPY?7!~~~~!7?J5B&@@@#BBBBB:                              .!5#@@@&B5J?7!~~~~!7?YPB&@@@#5!.          
+//            :J#@@@GJ!^................:~JG##BBBG.                            ^Y&@@&GJ~:................^!JG@@@#J:        
+//          .Y&@@#J^........................^?P&@&5^                         ^5@@@B?^........................^J#@@&Y.      
+//         7#@@#?:........:^~7????7~^:........:7B@@&?                       ?&@@B7.........:^~7????7~^:........:?#@@#7     
+//        Y@@@Y:.......^75B&@@@@@@@@@#P?^........J&@@P.        ...        .P@@&J........^?P#@@@@@@@@@&B57^.......:Y@@@Y    
+//       5@@@7.......~5&@@@BPY????J5B&@@&P~...... ~#@@G7J5PGBB#####BBGP5J7G@@#~ ......~P&@@&B5J????YPB@@@&5~.......7@@@5   
+//      ?@@@7.......J&@@BJ!^^^^^^^^^^~?B@@@Y:..^!JP&@@@@@@&&#BBBGBBB#&&@@@@@@&PJ!^..:Y@@@B?~^^^^^^^^^^!JB@@&J.......7@@@?  
+//     :&@@5.......5@@@Y^^^^^^^^^^^^^^^^J&@@GYB&@@@#G5J7!^^::.......::^^!7J5G#@@@&BYG@@&J^^^^^^^^^^^^^^^^Y@@@5.......5@@&: 
+//     ?@@@~......7@@@Y^^^^^^^^^^^^^^^^^^5@@@@@B5?~:.........................:~?5B@@@@@5^^^^^^^^^^^^^^^^^^Y@@@7......~@@@? 
+//     5@@#:......P@@#~^^^^^^^^^^^^^^^75&@@&G?~:.................................:~?G&@@&57^^^^^^^^^^^^^^^~#@@P......:#@@5 
+//     5@@#:......P@@#^^^^^^^^^^^^^^7G@@@BJ^.........................................^JB@@@G7^^^^^^^^^^^^^^#@@P......:#@@5 
+//     ?@@&~......?@@@?^^^^^^^^^^^!G@@@G!:.............................................:!G@@@G!^^^^^^^^^^^?@@@?......~&@@? 
+//     :&@@Y.......P@@&?^^^^^^^^~5@@@G!...................................................!G@@@5~^^^^^^^^?&@@P.......Y@@&: 
+//      J@@@!......:5@@@G?~^^^^7B@@&?.......................................................?&@@B!^^^^~?G@@@5:......!@@@J  
+//       P@@&!.......!G@@@#PY?J&@@B~.........................................................~B@@&J?YP#@@@G!.......!&@@P   
+//       .P@@&?........~JG&@@@@@@P:...........................................................:P@@@@@@&GJ~........?&@@P.   
+//         J&@@G!.........^~5@@@P...............................................................P@@@Y~^.........!G@@&J     
+//          ^P@@@G7:.....::^B@@G:...............................................................:G@@B:.......:7G@@@P^      
+//            ~5&@@&P7^:.::5@@&~.................................................................~&@@Y ..:^7P&@@&5~        
+//              :?G&@@@#PYJ&@@5................^!77~:.......................:~77!^................5@@&JYP#@@@&G?:          
+//                 :!JG#@@@@@@~..............!G&@@@@#J:...................:J#@@@@&G!..............~@@@@@@#GJ!:             
+//                     .:~G@@#:.............^@@@G7Y@@@?...................?@@@Y7G@@@^.............:#@@G~:.                 
+//                        G@@G..............^5B5:..JGP!.....:~7?J?7~:.....!PGJ..:5B5^..............G@@G                    
+//                        G@@G............................!P#@@@@@@@#P!............................G@@G                    
+//                        P@@B:.........................:5@@@BY?7?YB@@@5:.........................:B@@P                    
+//              ~!:       7@@@~...........^7?????????!:.Y@@&?^^^^^^^?&@@Y.:!?????????7^...........~@@@7                    
+//             7@@#.      :#@@P..........:G@@@@@@@@@@&~.G@@B^^^^^^^^^B@@G.~&@@@@@@@@@@G:..........P@@#:                    
+//             J@@&:       ?@@@7..........^!7777777!!^..J@@@J^^^^^^^J@@@J..^!!7777777!^..........7@@@?                     
+//             J@@&:        5@@&7...............  :!YGP~.Y@@@#5J?J5#@@@Y.~PGY!:  ...............7&@@5                      
+//             J@@&:         5@@@J:............^?P#@@@#~..~YB@@@@@@@BY~..~#@@@#P?^............:J@@@5                       
+//             ?@@&^          ?&@@B7........^JG@@@&GJ~......:^!777!^:......~JG&@@@GJ^........7B@@&?                        
+//             !@@@!           :5@@@B?^.....Y@@#57^...........................^75#@@Y.....^?B@@@5:                         
+//             .#@@Y             ^5&@@&P?^...^~:.................................:~^...^?P&@@&5^                           
+//              Y@@&:              .7P&@@@B5?~^:.................................:^~?5B@@@&P7.                             
+//              :#@@P                 .!JB@@@@@#G5J?7~~^:::::.......:::::^~~7?J5G#@@@@@BJ!.                                
+//               !@@@J                  :B@@#5G#&@@@@@@@&&##BBBBBBBBB##&&@@@@@@@&#G5#@@B:                                  
+//                ?@@@J                :#@@B^::^^~!7?Y5PGGB###########BGGP5Y?7!~^^::^B@@#:                                 
+//                 7@@@5.             .G@@#^....^?YPP5J!:::^^^^^^^^^^^:::!J5PP5?^....^#@@G.                                
+//                  ~#@@#!            ?@@@!...:Y&@@@&@@@G^.:::::::::::.~G@@@&@@@&Y:...!@@@?                                
+//                   .5@@@P^          B@@G....J@@&7:.~#@@B.............B@@B~.:7&@@J....G@@B                                
+//                     ~G@@@P~       :&@@J....5@@&.   !@@@5:.........:5@@@!   .&@@5....J@@&:                               
+//                       ~P@@@BJ^    ^@@@?....~@@@5    7#@@#7.......7#@@#7    5@@@~....?@@@^                               
+//                         ^J#@@@BJ~..#@@Y.....?@@@5.   .Y@@@!.....!@@@Y.   .5@@@?.....Y@@&:                               
+//                           .~YB@@@&G&@@#:.....7#@@#7.  ?@@@7.....7@@@?  .7#@@#7.....:#@@P                                
+//                               ^75B&@@@@5......^Y&@@&B#@@@Y:.....:Y@@@#B&@@&Y^......5@@&^                                
+//                                   .^?&@@P^......^?PBBBPJ~.........~JPBBBP?^......^P@@&!                                 
+//                                     .P@@@#J^...................................^J#@@@P.                                 
+//                                    !&@@#&@@&57^.............................^75&@@&#@@&!                                
+//                                   .#@@G^~JB&@@&B5J!~^::.............::^~!J5B&@@&BJ~^G@@#.                               
+//                                    P@@#J7!7J5B@@@@@@&#BBGPP55555PPGBB#&@@@@@@B5J7!7J#@@P                                
+//                                    .Y&@@@@@@@@@@@@@@@@@@&&@@@@@@@&&@@@@@@@@@@@@@@@@@@&Y.                                
+//                                      .~?JJJJJJJJJJJJJ?!:..:::::::..:!?JJJJJJJJJJJJJ?~. 
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract JieFangXS is ERC20, ERC20Burnable, Ownable {
+    IERC20 _xsToken;
+
+    // Attributes
+    bool    public isMintableOrNot = false;
+    address public devAddr;
+
+    constructor() ERC20("JieFangXS", "JFXS") {
+        devAddr  = msg.sender;
+        _xsToken = IERC20(0x442517Ee1fE6Bae7F34C7d384398cC38d9D8a578);
+    }
+
+    function toggleMintStatus() public onlyOwner {
+        isMintableOrNot = !isMintableOrNot;
+    }
+
+    function justDonateToDev() external payable {
+        (bool donateSuccess, ) = devAddr.call{value: msg.value}("");
+        require(donateSuccess, "Donate failed");
+    }
+
+    modifier checkAllowance(uint amount) {
+        require(_xsToken.allowance(msg.sender, address(this)) >= amount, "Pls approve XSB allowance");
+        _;
+    }
+
+    function jieFangXS(uint _amount) public checkAllowance(_amount) {
+        require(isMintableOrNot, "JieFangXS is not mintable now");
+        _xsToken.transferFrom(msg.sender, address(this), _amount);
+        _mint(msg.sender, _amount);
+    }
+
+    function withdrawXSB(address to, uint amount) public onlyOwner {
+        _xsToken.transfer(to, amount);
+    }
+
+    function getContractXSB_Balance() external view returns(uint) {
+        return _xsToken.balanceOf(address(this));
+    }
+
+    function getXSB_Allowance(address _to) external view returns(uint) {
+        return _xsToken.allowance(msg.sender, _to);
+    }
+
+    function devMint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
